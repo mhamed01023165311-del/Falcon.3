cebc95a56198d73eca8adb2983a7fe75d71e8f3f
Falcon.1 <mhamed01023165311@gmail.com>
2025-11-27 16:20:39 +0200

    Add comments in Arabic to App.tsx
    Added comments in Arabic for clarity.

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+// استدعاء المساعد البصري الجديد
 import VisualAssistant from './components/VisualAssistant';
 
 const App: React.FC = () => {
   return (
+    // خلفية سوداء للتطبيق
     <div className="min-h-screen bg-black text-white font-['Cairo']">
       <VisualAssistant />
     </div>
