83ae9576ad3bff3cbf9b4e5ca093d122bb401f2b
Falcon.1 <mhamed01023165311@gmail.com>
2025-11-27 16:13:27 +0200

    Refactor App component to use React.FC type

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import VisualAssistant from './components/VisualAssistant';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="min-h-screen bg-black">
+    <div className="min-h-screen bg-black text-white font-['Cairo']">
       <VisualAssistant />
     </div>
   );
-}
+};
 
 export default App;
